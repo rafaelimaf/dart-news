3c517ff8ca7ecb3e24632e6ee337aa2e5ab6bee7
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-12-30 10:08:07 -0300

    feat: algumas práticas com queryParams no método get

--- bin/router.dart
+++ bin/router.dart
@@ -5,9 +5,15 @@ class RouterHandler {
   Handler get handler {
     final router = Router();
 
-    router.get('/', (Request request) {
+    router.get('/', (Request req) {
       return Response(200, body: 'Hello World!');
-    }); 
+    });
+
+    router.get('/query', (Request req) {
+      String? user = req.url.queryParameters["user"];
+
+      return Response(200, body: 'Hello $user!');
+    });
 
     return router;
   }
