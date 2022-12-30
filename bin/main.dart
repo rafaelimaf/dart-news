06529d317fd57504ace28e08a4b8679a1e7b153c
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-12-30 09:48:20 -0300

    feat: implementação de router com shelf_router

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,11 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
+import 'router.dart';
 
 void main() async {
-  await shelf_io.serve((request) => Response(200, body: 'Hello World!'), 'localhost', 8080);
+  var router = RouterHandler();
+
+  await shelf_io.serve(router.handler, 'localhost', 8080);
 
   print('Server is up on http://localhost:8080');
 }
