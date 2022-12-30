634f7f0c482859aefc1e8565a7ea69357ae846ca
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-12-30 13:48:43 -0300

    feat: implementação base do CRUD de notícias

--- src/controllers/blog.controller.dart
+++ src/controllers/blog.controller.dart
@@ -1,7 +1,22 @@
+import 'dart:convert';
 import 'package:shelf/shelf.dart';
 
 class BlogController {
-  Future<Response> getNews(Request req) async {
-    return Response(200, body: "[]");
+  Future<Response> createNews(Request req) async {
+    return Response(201);
+  }
+  
+  Future<Response> readNews(Request req) async {
+    return Response(200, body: jsonEncode([]), headers: {"content-type": "application/json"});
+  }
+
+  Future<Response> updateNews(Request req, String id) async {    
+    return Response(200, body: jsonEncode({
+      "message": "Você editou a notícia $id"
+    }));
+  }
+
+  Future<Response> deleteNews(Request req, String id) async {    
+    return Response(204);
   }
 }
--- src/routes/blog.router.dart
+++ src/routes/blog.router.dart
@@ -1,13 +1,18 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
-
 import '../controllers/blog.controller.dart';
 
 class BlogRouter {
   Handler get handler {
     final router = Router();
 
-    router.get('/news', BlogController().getNews);
+    router.post('/news', BlogController().createNews);
+
+    router.get('/news', BlogController().readNews);
+
+    router.put('/news/<id>', BlogController().updateNews);
+
+    router.delete('/news/<id>', BlogController().deleteNews);
 
     return router;
   }
