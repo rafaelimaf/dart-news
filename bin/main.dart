75028919e96b15d7f12bc9472ccdb4cd425539e6
Rafael de Lima <rafaelima.fernandez@gmail.com>
2022-12-30 09:24:17 -0300

    feat: imlementação do shelf server middleware e primeira implementação do server

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,8 @@
-void main() {
-  
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
+
+void main() async {
+  await shelf_io.serve((request) => Response(200, body: 'Hello World!'), 'localhost', 8080);
+
+  print('Server is up on http://localhost:8080');
 }
